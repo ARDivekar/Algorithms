8040e73bc778749d286ff56d02d309051577b9d4
ARDivekar <abhishek.r.divekar@gmail.com>
2016-08-14 21:56:20 +0530

    Added datatype limits for long long and unsigned long long

---
+++ C++/Library Tests/C++ datatype limits.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<limits.h>
+#include<math.h>
+
+using namespace std;
+
+void testUnsignedLongLong(){
+    unsigned long long data;
+    data = ULLONG_MAX;
+    cout<<"\n\nunsigned long long:";
+    cout<<"\n\tMaximum value (from ULLONG_MAX):  "<<data;
+    data += 1;
+    cout<<"\n\tWraparound value: \t\t  "<<data;
+    data += 1;
+    cout<<"\n\t(Wraparound value + 1): \t  "<<data;
+    data += 1;
+    cout<<"\n\t(Wraparound value + 2): \t  "<<data;
+    cout<<endl;
+}
+
+void testLongLong(){
+    long long data;
+    data = LLONG_MAX;
+    cout<<"\n\nlong long:";
+    cout<<"\n\tMaximum value (from LLONG_MAX):  "<<data;
+    data += 1;
+    cout<<"\n\tWraparound value: \t\t"<<data;
+    data += 1;
+    cout<<"\n\t(Wraparound value + 1): \t"<<data;
+    data += 1;
+    cout<<"\n\t(Wraparound value + 2): \t"<<data;
+    cout<<endl;
+}
+
+
+
+int main(){
+    testUnsignedLongLong();
+    testLongLong();
+    cout<<endl;
+    return 0;
+}
